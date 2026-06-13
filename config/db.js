c2119160ffb08916a63949cd980af4448eb88b78
nadiaanndtya <nadiaaninditiya@gmail.com>
2026-06-13 18:31:56 +0800

    Remove debug logs

--- config/db.js
+++ config/db.js
@@ -1,13 +1,5 @@
 const { Sequelize } = require("sequelize");
 
-console.log("DATABASE_URL =", process.env.DATABASE_URL);
-
-try {
-  console.log("PG MODULE =", require.resolve("pg"));
-} catch (err) {
-  console.error("PG NOT FOUND", err);
-}
-
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
