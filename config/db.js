0867ab0b329dcd6fe7129b99cdde31a27036f031
nadiaanndtya <nadiaaninditiya@gmail.com>
2026-06-13 18:48:39 +0800

    debug pg

--- config/db.js
+++ config/db.js
@@ -1,3 +1,11 @@
+console.log("START DB");
+
+try {
+  console.log("PG PATH =", require.resolve("pg"));
+} catch (err) {
+  console.error("PG NOT FOUND", err);
+}
+
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
