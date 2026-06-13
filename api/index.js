be6da38a811e1cf3b3593653614b436736af64a1
nadiaanndtya <nadiaaninditiya@gmail.com>
2026-06-13 18:16:25 +0800

    fix vercel startup

--- api/index.js
+++ api/index.js
@@ -1,3 +1,14 @@
+require("dotenv").config();
+
+const sequelize = require("../config/db");
 const app = require("../app");
+
+sequelize.authenticate()
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.error("Database error:", err);
+  });
 
 module.exports = app;
